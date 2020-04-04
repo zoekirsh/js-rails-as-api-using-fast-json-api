a56d46eaec58c73855567848ff68f9af8da35b74
Liz Burton <liz.burton147@gmail.com>
2020-04-04 13:32:22 -0400

    Update sightings_controller.rb to match README

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,6 +1,6 @@
 class SightingsController < ApplicationController
   def show
-    @sighting = Sighting.find(params[:id])
-    render json: @sighting.to_json(:include => {:bird => {:only =>[:name, :species]}, :location => {:only =>[:latitude, :longitude]}}, :except => [:updated_at])
+    sighting = Sighting.find(params[:id])
+    render json: sighting.to_json(:include => {:bird => {:only =>[:name, :species]}, :location => {:only =>[:latitude, :longitude]}}, :except => [:updated_at])
   end
 end
